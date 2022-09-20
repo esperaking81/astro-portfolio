ef20e7a9dd2ccb7bb9e9439853706bab94cbfc4f
Esperah AWO <esperah.awo@mehriso.com>
2022-09-20 19:38:42 +0100

    build(dep): add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import image from "@astrojs/image";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), image()]
+  integrations: [react(), image(), tailwind()]
 });
