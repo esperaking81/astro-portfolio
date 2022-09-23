8330893820a827159e6d234702efdf5efecadc62
Esperah AWO <esperah.awo@mehriso.com>
2022-09-23 07:27:08 +0100

    feat(nav): style nav bar

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const HOMEPAGE_URL = 'https://www.esperaawo.me';
+export const SITE_TITLE = 'Espera Awo: Personal Website';
+export const SITE_DESCRIPTION = 'All about me and my work';
+
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+      fontFamily: {
+        mplus: ["'M PLUS Rounded 1c'", 'Verdana', 'sans-serif'],
+        quicksand: ['Quicksand', 'Verdana', 'sans-serif'],
+      }
+    },
 	},
 	plugins: [],
 }
