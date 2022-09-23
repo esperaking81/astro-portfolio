1a874802cb795cad262973b9a318ca210749385f
Esperah AWO <esperah.awo@mehriso.com>
2022-09-23 14:49:12 +0100

    feat(work): add work page

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
 export const HOMEPAGE_URL = 'https://www.esperaawo.me';
 export const SITE_TITLE = 'Espera Awo: Personal Website';
 export const SITE_DESCRIPTION = 'All about me and my work';
-
---
+++ src/models/work.ts
@@ -0,0 +1,4 @@
+export type Work = {
+  title: String
+  description: String;
+}
